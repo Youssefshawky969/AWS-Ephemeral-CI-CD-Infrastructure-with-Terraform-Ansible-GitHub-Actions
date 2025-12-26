6b8d9e7504812e2893e8e40e131257da6126aaa1
Youssef Shawky <168371773+Youssefshawky969@users.noreply.github.com>
2025-12-26 21:12:24 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = var.ami_id
   instance_type = var.instance_type
-  key_name = aws_key_pair.ci_key.key_name
+  key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
@@ -11,6 +11,7 @@ resource "aws_instance" "web" {
   }
 
 }
+
 
 
 
